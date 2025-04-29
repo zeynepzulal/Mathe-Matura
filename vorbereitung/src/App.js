a1e2dce132ce9b0c3fdc5d54288468c9a2a92e44
Zeynep Zülal <119702784+zeynepzulal@users.noreply.github.com>
2025-04-29 16:23:35 +0200

    pages

--- vorbereitung/src/App.js
+++ vorbereitung/src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Topic from "./components/Topic";
+import "./App.css"; 
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <div className="app-container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/topic/:topicId" element={<Topic />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
